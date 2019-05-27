d2a6ccc3b98fb507a38313a6c54fa6b7a7411f71
niveshgandhi <48728534+niveshgandhi@users.noreply.github.com>
2019-05-27 06:32:40 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,4 +1,34 @@
 #include <stdio.h>
+#include <math.h>
+
+int main () {
+  
+  char fromUnit, toUnit;    // 
+  char *fUnit, *tunit;    //
+  long double fromValue, meterValue, toValue;   //
+  int power = 0;
+  
+  printf("to convert the 12 Inch to Foot\n");
+  printf("enter the unit in the format : dc12\n");
+  
+  printf("Ell: a\n");
+  printf("Femi: b\n");
+  printf("Foot; c\n");
+  printf("Inch: d\n");
+  printf("Light year: e\n");
+  printf("Meter: f\n");
+  printf("Mile: g\n");
+  printf("Nano meter: h\n");
+  printf("Pace: i\n");
+  printf("Point: j\n");
+  printf("Yard: k\n");
+  printf("Mile meter: l\n");
+  printf("Centi meter: m\n");
+  printf(":\n");
+  printf(":\n");
+  
+  
+}
 
 typedef struct _unit {
   char * name;
